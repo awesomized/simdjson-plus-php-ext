b50e6c073516dd946b6186f0039c7affc359b2dc
Jakub Onderka <jakub.onderka@gmail.com>
2024-12-15 15:43:20 +0100

    Change version to 5.0.0dev

--- php_simdjson.h
+++ php_simdjson.h
@@ -51,15 +51,15 @@ BEGIN_EXTERN_C()
 extern zend_module_entry simdjson_module_entry;
 #define phpext_simdjson_ptr &simdjson_module_entry
 
-#define PHP_SIMDJSON_VERSION                  "4.0.1dev"
+#define PHP_SIMDJSON_VERSION                  "5.0.0dev"
 /**
  * PHP_SIMDJSON_VERSION_ID has the same format as PHP_VERSION_ID: Major version * 10000 + Minor version * 100 + Patch version.
  * This is meant for use by PECL extensions that depend on simdjson.
  * (e.g. 4.5.6dev and 4.5.6 would be 40506)
  */
-#define PHP_SIMDJSON_VERSION_ID               40001
+#define PHP_SIMDJSON_VERSION_ID               50000
 
-#define SIMDJSON_SUPPORT_URL                  "https://github.com/crazyxman/simdjson_php"
+#define SIMDJSON_SUPPORT_URL                  "https://github.com/JakubOnderka/simdjson_php"
 
 #define SIMDJSON_PARSE_DEFAULT_DEPTH          512
 
